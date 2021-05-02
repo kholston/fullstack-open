a760189a55a4e21a5d5e470cb047c7f04885f0c1
Ken Holston <git.kholston@gmail.com>
2021-05-02 11:24:02 -0400

    correct exercise 1.1

--- part1/course-information/src/App.js
+++ part1/course-information/src/App.js
@@ -6,7 +6,11 @@ const Header = (props) =>{
 
 const Content = (props) =>{
   return(
-    <p>{props.part} {props.exercises}</p>
+    <div>
+      <p>{props.parts[0]} {props.exercises[0]}</p>
+      <p>{props.parts[1]} {props.exercises[1]}</p>
+      <p>{props.parts[2]} {props.exercises[2]}</p>
+    </div>
   )
 }
 
@@ -26,12 +30,13 @@ function App() {
   const part3 = 'State of a component';
   const exercises3 = 14;
 
+  const parts = [part1, part2, part3];
+  const exercises = [exercises1, exercises2, exercises3];
+  
   return (
     <div>
       <Header course={course}/>
-      <Content part={part1} exercises={exercises1}/>
-      <Content part={part2} exercises={exercises2}/>
-      <Content part={part3} exercises={exercises3}/>
+      <Content parts={parts} exercises={exercises}/>
       <Total total={exercises1 + exercises2 + exercises3}/>
     </div>
   );
