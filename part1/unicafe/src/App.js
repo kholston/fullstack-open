734135048a028b244708d4e264e5c38351f62e4d
Ken Holston(Work) <git.kholston@gmail.com>
2021-05-05 04:20:28 +0000

    exercise 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -12,16 +12,31 @@ const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
   
   const handleGoodClick = () =>{
     setGood(good + 1)
+    setAll(all + 1)
   }
   const handleNeutralClick = () =>{
     setNeutral(neutral + 1)
+    setAll(all+ 1)
   }
   const handleBadClick = () =>{
     setBad(bad + 1)
+    setAll(all+ 1)
   }
+  
+ 
+  const getAverage = () => {
+    return ((good + (-bad) )/all)
+  }
+  
+  const getPositive = () =>{
+    if(all === 0) return 'Give Feedback To See Percentage';
+    return (((good * 100)/all) + '%');
+  }
+  
   
   return (
     <div>
@@ -36,6 +51,9 @@ const App = () => {
       <Display name='Good' value={good}/>
       <Display name='Neutral' value={neutral}/>
       <Display name='Bad' value={bad}/>
+      <Display name='All' value={all}/>
+      <Display name='Average' value={getAverage()}/>
+      <Display name='Positive' value={getPositive()}/>
     </div>
   );
 }
