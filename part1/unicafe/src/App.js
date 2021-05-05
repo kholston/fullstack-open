9177eeace7fdf2f51efd3b1b4b1edbb5d91afd91
Ken Holston <git.kholston@gmail.com>
2021-05-05 12:20:21 -0400

    exercise 1.8

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -8,6 +8,33 @@ const Button = ({handleClick, text}) => (
   <button onClick={handleClick}>{text}</button>  
 )
 
+const Statistics = (props) =>{
+  const {good,neutral,bad,all} = props.stats;
+  
+  const getAverage = () => {
+    if(all === 0) return 'Give Feedback To See Average';
+    return ((good + (-bad) )/all)
+  }
+  
+  const getPositive = () =>{
+    if(all === 0) return 'Give Feedback To See Percentage';
+    return (((good * 100)/all) + '%');
+  }
+  
+  return (
+    <div>
+      <Header title='Statistics'/>
+      
+      <Display name='Good' value={good}/>
+      <Display name='Neutral' value={neutral}/>
+      <Display name='Bad' value={bad}/>
+      <Display name='All' value={all}/>
+      <Display name='Average' value={getAverage()}/>
+      <Display name='Positive' value={getPositive()}/>
+    </div>
+  )
+}
+
 const App = () => {
   const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
@@ -28,15 +55,7 @@ const App = () => {
   }
   
  
-  const getAverage = () => {
-    return ((good + (-bad) )/all)
-  }
-  
-  const getPositive = () =>{
-    if(all === 0) return 'Give Feedback To See Percentage';
-    return (((good * 100)/all) + '%');
-  }
-  
+
   
   return (
     <div>
@@ -45,15 +64,8 @@ const App = () => {
       <Button handleClick={handleGoodClick} text='Good'/>
       <Button handleClick={handleNeutralClick} text='Neutral'/>
       <Button handleClick={handleBadClick} text='Bad'/>
+      <Statistics stats={{good,neutral,bad,all}}/>
       
-      <Header title='Statistics'/>
-      
-      <Display name='Good' value={good}/>
-      <Display name='Neutral' value={neutral}/>
-      <Display name='Bad' value={bad}/>
-      <Display name='All' value={all}/>
-      <Display name='Average' value={getAverage()}/>
-      <Display name='Positive' value={getPositive()}/>
     </div>
   );
 }
