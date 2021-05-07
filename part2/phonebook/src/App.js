4fd052cda8dff8413b1d102939c1d37e92af3bb4
Ken Holston(Work) <git.kholston@gmail.com>
2021-05-07 18:29:15 +0000

    exercise 2.6

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -5,20 +5,37 @@ const App = () => {
     { name: 'Arto Hellas' }
   ]) 
   const [ newName, setNewName ] = useState('')
+  
+  const handleNameChange = (event) => {
+    setNewName(event.target.value);
+  }
+  
+  const addName = (event) => {
+    event.preventDefault();
+    const personObject = {name: newName}
+    if(persons.some(person => person.name === personObject.name)){
+      alert(`${newName} is already in the phonebook.`)
+    } else {
+      setPersons(persons.concat(personObject))
+    }
+    setNewName('')
+  } 
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form>
+      <form onSubmit={addName}>
         <div>
-          name: <input />
+          name: <input onChange={handleNameChange} value={newName}/>
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
+      <ul>
+        {persons.map(person => <li key={person.name}>{person.name}</li>)}
+      </ul>      
     </div>
   )
 }
