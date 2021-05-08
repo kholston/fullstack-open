da6df9b70399e98ebce982ecbbeedc2e9e250077
Ken Holston <git.kholston@gmail.com>
2021-05-07 23:26:40 -0400

    exercise 2.7 and  2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -5,37 +5,44 @@ const App = () => {
     { name: 'Arto Hellas' }
   ]) 
   const [ newName, setNewName ] = useState('')
-  
+  const [newNumber, setNewNumber] = useState('')
+
   const handleNameChange = (event) => {
-    setNewName(event.target.value);
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
   }
   
-  const addName = (event) => {
+  const addPerson = (event) => {
     event.preventDefault();
-    const personObject = {name: newName}
+    const personObject = {name: newName, number: newNumber}
     if(persons.some(person => person.name === personObject.name)){
       alert(`${newName} is already in the phonebook.`)
     } else {
       setPersons(persons.concat(personObject))
     }
     setNewName('')
+    setNewNumber('')
   } 
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addName}>
+      <form onSubmit={addPerson}>
         <div>
           name: <input onChange={handleNameChange} value={newName}/>
+        </div>
+        <div>
+          number: <input onChange={handleNumberChange} value={newNumber} />
         </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      <ul>
-        {persons.map(person => <li key={person.name}>{person.name}</li>)}
-      </ul>      
+        {persons.map(person => <p key={person.name}>{person.name} {person.number}</p>)}   
     </div>
   )
 }
