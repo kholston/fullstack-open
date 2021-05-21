605ff931113370a32f1ed7bfacc04bf402797106
Ken Holston <git.kholston@gmail.com>
2021-05-20 20:49:57 -0400

    update to match backend

--- part2/notes/src/App.js
+++ part2/notes/src/App.js
@@ -23,9 +23,7 @@ const App = () =>{
     event.preventDefault()
     const noteObject = {
       content: newNote,
-      date: new Date().toISOString(),
-      important: Math.random() < 0.5,
-      id: notes.length + 1
+      important: Math.random() < 0.5, 
     }
     
     noteService
