0a8f3c3a70abc309200976f88c9ea894e726ab24
Ken Holston <git.kholston@gmail.com>
2021-07-06 14:14:47 -0400

    section 6 action creators

--- part6/redux-notes/src/index.js
+++ part6/redux-notes/src/index.js
@@ -4,10 +4,9 @@ import { createStore } from 'redux'
 import noteReducer from './reducers/noteReducer';
 import './index.css';
 
-
-
 const store = createStore(noteReducer)
 
+// create initial notes
 store.dispatch({
   type: 'NEW_NOTE',
   data: {
@@ -26,12 +25,55 @@ store.dispatch({
   }
 })
 
+// generate id for new notes
+const generateId = () => 
+  Math.floor(Math.random() * 1000000)
+
+
+const createNote = (content) => {
+  return {
+    type: 'NEW_NOTE',
+    data: {
+      content,
+      important: false,
+      id: generateId()
+    }
+  }
+}
+
+const toggleImportanceOf = (id) => {
+  return {
+    type: "TOGGLE_IMPORTANCE",
+    data: { id }
+  }
+} 
+
+
+
 const App = () => {
+  const addNote = (event) => {
+    event.preventDefault()
+    const content = event.target.note.value
+    event.target.note.value = ''
+    store.dispatch(createNote(content))
+  }
+
+  const toggleImportance = (id) => {
+    store.dispatch(toggleImportanceOf(id))
+  }
+
   return (
     <div>
+      <form onSubmit={addNote}>
+        <input name="note" />
+        <button type="submit">add</button>
+      </form>
       <ul>
         {store.getState().map(note => 
-          <li key={note.id}>
+          <li 
+            key={note.id}
+            onClick={() => toggleImportance(note.id)}
+          >
             {note.content} <strong>{note.important ? 'important': ''}</strong>
           </li>
         )}
