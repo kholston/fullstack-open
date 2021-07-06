0b2faf2f7a99daf3437864073f90206437318c20
Ken Holston <git.kholston@gmail.com>
2021-07-06 14:46:40 -0400

    section 6a add react-redux

--- part6/redux-notes/src/index.js
+++ part6/redux-notes/src/index.js
@@ -1,90 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore } from 'redux'
+import { Provider } from 'react-redux'
+import App from './App'
 import noteReducer from './reducers/noteReducer';
 import './index.css';
 
 const store = createStore(noteReducer)
 
-// create initial notes
-store.dispatch({
-  type: 'NEW_NOTE',
-  data: {
-    content: 'the app state is in redux store',
-    important: true,
-    id: 1
-  }
-})
-
-store.dispatch({
-  type: 'NEW_NOTE',
-  data: {
-    content: 'state changes are made with actions',
-    important: false,
-    id: 2
-  }
-})
-
-// generate id for new notes
-const generateId = () => 
-  Math.floor(Math.random() * 1000000)
-
-
-const createNote = (content) => {
-  return {
-    type: 'NEW_NOTE',
-    data: {
-      content,
-      important: false,
-      id: generateId()
-    }
-  }
-}
-
-const toggleImportanceOf = (id) => {
-  return {
-    type: "TOGGLE_IMPORTANCE",
-    data: { id }
-  }
-} 
-
-
-
-const App = () => {
-  const addNote = (event) => {
-    event.preventDefault()
-    const content = event.target.note.value
-    event.target.note.value = ''
-    store.dispatch(createNote(content))
-  }
-
-  const toggleImportance = (id) => {
-    store.dispatch(toggleImportanceOf(id))
-  }
-
-  return (
-    <div>
-      <form onSubmit={addNote}>
-        <input name="note" />
-        <button type="submit">add</button>
-      </form>
-      <ul>
-        {store.getState().map(note => 
-          <li 
-            key={note.id}
-            onClick={() => toggleImportance(note.id)}
-          >
-            {note.content} <strong>{note.important ? 'important': ''}</strong>
-          </li>
-        )}
-      </ul>
-    </div>
-  )
-}
-
-const renderApp = () => {
-  ReactDOM.render(<App/>, document.getElementById('root'))
-}
-
-renderApp()
-store.subscribe(renderApp)
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+)
