818b45755e700835847bf16a6504b640311536a8
Ken Holston <git.kholston@gmail.com>
2021-08-21 16:31:49 -0400

    style bloglist

--- part7/bloglist-frontend/src/components/BlogList.js
+++ part7/bloglist-frontend/src/components/BlogList.js
@@ -1,6 +1,9 @@
 import React, { useRef } from 'react'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
+import {
+  ListGroup
+} from 'react-bootstrap'
 import Togglable from './Togglable'
 import BlogForm from './BlogForm'
 
@@ -12,13 +15,6 @@ const BlogList = () => {
   const toggleBlogForm = () => {blogFormRef.current.toggleVisibility()}
 
 
-  const linkStyle = {
-    paddingTop: 10,
-    paddingLeft: 2,
-    border: 'solid',
-    borderWidth: 1,
-    marginBottom: 5
-  }
 
   return(
     <div>
@@ -33,15 +29,21 @@ const BlogList = () => {
           </div>
         }
       </div>
-      { blogs &&
-        blogs
-          .sort((a,b) => b.likes - a.likes)
-          .map(blog =>
-            <div key={blog.id} style={linkStyle}>
-              <Link  to={`/blogs/${blog.id}`} >{blog.title}</Link>
-            </div>
-          )
-      }
+      <ListGroup>
+        { blogs &&
+          blogs
+            .sort((a,b) => b.likes - a.likes)
+            .map(blog =>
+              // <div key={blog.id} >
+              //   <Link  to={`/blogs/${blog.id}`} >{blog.title}</Link>
+              // </div>
+              <ListGroup.Item key={blog.id}>
+                <Link  to={`/blogs/${blog.id}`} >{blog.title}</Link>
+              </ListGroup.Item>
+            )
+        }
+      </ListGroup>
+
     </div>
   )
 }
