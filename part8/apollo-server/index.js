dd45cddf44495bf3ad141b0e964447bf438a5d09
Ken Holston <git.kholston@gmail.com>
2021-08-22 13:25:20 -0400

    add mutation for editing

--- part8/apollo-server/index.js
+++ part8/apollo-server/index.js
@@ -25,6 +25,11 @@ let persons = [
 ]
 
 const typeDefs = gql`
+  enum YesNo {
+    YES
+    NO
+  }
+
   type Address {
     street: String!
     city: String!
@@ -39,7 +44,7 @@ const typeDefs = gql`
 
   type Query {
     personCount: Int!
-    allPersons: [Person!]!
+    allPersons(phone: YesNo): [Person!]!
     findPerson(name: String!) : Person
   }
 
@@ -50,13 +55,24 @@ const typeDefs = gql`
       street: String!
       city: String
     ): Person
+    editNumber(
+      name: String!
+      phone: String!
+    ): Person
   }
 `
 
 const resolvers = {
   Query: {
     personCount: () => persons.length,
-    allPersons: () => persons,
+    allPersons: (root, args) => {
+      if(!args.phone) {
+        return persons
+      }
+      const byPhone = (person) => 
+        args.phone === 'YES' ? person.phone : !person.phone
+      return persons.filter(byPhone)
+    },
     findPerson: (root, args) => 
       persons.find(p => p.name === args.name)
   },
@@ -80,6 +96,17 @@ const resolvers = {
       persons = persons.concat(person)
       return person
     }
+   },
+
+   editNumber: (root,args) => {
+     const person = persons.find(p => p.name === args.name)
+     if(!person){
+       return null
+     }
+
+     const updatedPerson = { ...person, phone: args.phone }
+     persons = persons.map(p => p.name === args.name ? updatedPerson: p)
+     return updatedPerson
    }
 }
 
