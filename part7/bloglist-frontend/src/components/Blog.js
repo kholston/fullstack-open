1ce52df20042b4bb9dcc292b2758620020e23865
Ken Holston <git.kholston@gmail.com>
2021-08-21 21:48:21 -0400

    style blog

--- part7/bloglist-frontend/src/components/Blog.js
+++ part7/bloglist-frontend/src/components/Blog.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { Button, Card, Form, ListGroup } from 'react-bootstrap'
 import { useParams } from 'react-router-dom'
 import { likeBlog, addComment } from '../reducers/blogReducer'
 
@@ -44,8 +45,8 @@ const Blog = () => {
 
   return(
     blog &&
-    <div className="blogDiv">
-      <h2>{blog.title} {blog.author}</h2>
+    <div>
+      {/* <h2>{blog.title} {blog.author}</h2>
       <a href={blog.url}>{blog.url}</a>
       <div>
         {blog.likes} likes
@@ -61,7 +62,29 @@ const Blog = () => {
         {blog.comments.map(comment => (
           <li key={comment}>{comment}</li>
         ))}
-      </ul>
+      </ul> */}
+      <Card className='col-sm-8 offset-sm-2 offset-4 mt-3'>
+        <Card.Body>
+          <Card.Title>{blog.title}</Card.Title>
+          <Card.Subtitle className='mb-3 text-muted'>{blog.author}</Card.Subtitle>
+          <Card.Link href={`${blog.url}`}>{blog.url}</Card.Link>
+          <Card.Text>{blog.likes} likes</Card.Text>
+          <Button onClick={() => addLike(blog) }>like</Button>
+        </Card.Body>
+      </Card>
+      <h3 className='mt-3 offset-lg-4'>comments</h3>
+      <Form onSubmit={submitComment} className='col-lg-4 offset-lg-4 mb-3'>
+        <Form.Group>
+          <Form.Label>comment</Form.Label>
+          <Form.Control type='text' value={comment} onChange={handleCommentChange}/>
+        </Form.Group>
+        <Button type='submit'>add comment</Button>
+      </Form>
+      <ListGroup className='col-lg-6 offset-lg-3'>
+        {blog.comments.map(comment => (
+          <ListGroup.Item key={comment}>{comment}</ListGroup.Item>
+        ))}
+      </ListGroup>
     </div>
   )
 }
