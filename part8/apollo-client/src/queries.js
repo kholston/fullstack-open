5615fc7e176b96a4dfd3b82c717e556882af5e1e
Ken Holston <git.kholston@gmail.com>
2021-12-16 23:20:31 -0500

    add PersonDetails fragment to queries

--- part8/apollo-client/src/queries.js
+++ part8/apollo-client/src/queries.js
@@ -1,5 +1,18 @@
 import { gql } from '@apollo/client'
 
+
+const PERSON_DETAILS = gql`
+  fragment PersonDetails on Person {
+    id
+    name
+    phone
+    address {
+      street
+      city
+    }
+  }
+`
+
 export const CREATE_PERSON = gql`
 mutation createPerson ($name: String!, $street: String!, $city: String!, $phone: String) {
   addPerson(
@@ -8,53 +21,37 @@ mutation createPerson ($name: String!, $street: String!, $city: String!, $phone:
     city: $city
     phone: $phone
   ) {
-    name
-    phone
-    id
-    address {
-      city 
-      street
-    }
+    ...PersonDetails
   }
 }
+${PERSON_DETAILS}
 `
 
 export const ALL_PERSONS = gql`
 query {
   allPersons {
-    name
-    phone
-    id
+    ...PersonDetails
   }
 }
+${PERSON_DETAILS}
 `
 
 export const FIND_PERSON = gql`
 query findPersonByName($nameToSearch: String!){
     findPerson(name: $nameToSearch) {
-      name
-      phone
-      id
-      address {
-        street
-        city
-      }
+      ...PersonDetails
     }
   }
+  ${PERSON_DETAILS}
 `
 
 export const EDIT_NUMBER = gql`
   mutation editNumber($name: String!, $phone: String) {
     editNumber(name: $name, phone: $phone) {
-      name
-      phone
-      address {
-        city
-        street
-      }
-      id
+      ...PersonDetails
     }
   }
+  ${PERSON_DETAILS}
 `
 export const LOGIN = gql`
   mutation login($username: String!, $password: String!) {
