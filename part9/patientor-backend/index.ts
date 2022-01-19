b814231c8715ab4a4c831f88f49a3ef5b199be46
Ken Holston <git.kholston@gmail.com>
2022-01-19 15:31:09 -0500

    exercise 9.9

--- part9/patientor-backend/index.ts
+++ part9/patientor-backend/index.ts
@@ -1,13 +1,22 @@
 import express from 'express';
+import cors from 'cors';
+
+const allowedOrigins: string[] = ['http://localhost:3000'];
+const options: cors.CorsOptions = {
+  origin: allowedOrigins
+};
+
 const app = express();
+// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+app.use(cors(options));
 app.use(express.json());
 
-app.get('/ping', (_req,res) => {
+app.get('/api/ping', (_req,res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
 
-const PORT = 3000;
+const PORT = 3001;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
